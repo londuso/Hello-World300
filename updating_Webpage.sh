9533c656b84b6feb4e9b6e5e70b6bef80469c2f1
londuso <44388336+londuso@users.noreply.github.com>
2019-10-11 16:09:43 -0700

    Update updating_Webpage.sh

--- updating_Webpage.sh
+++ updating_Webpage.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
 time=$(date)
 echo "<html><body><h1> Hi there ,it is time,how are you ? </h1></body><html>" > /var/www/html/index.html
+
+
+ur (0 - 23)
+# │ │ ┌───────────── day of the month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                   7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * * command to execute
+30 *  *  *  */path/to command
+
+30   17 * * * /path/to/command # will run at 5.30 p 
+ 
+00   6 * * 3  /path/to 
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+-- INSERT --                                                                                     14,24         All
+
+
